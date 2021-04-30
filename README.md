999fae6bf84fe9920a7487010fcbe2b42c99d410
Mathis Van Eetvelde <mathis.vaneetvelde@protonmail.com>
2021-04-30 10:32:51 +0200

    fmt and README.md badge
    Signed-off-by: Mathis Van Eetvelde <mathis.vaneetvelde@protonmail.com>

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # golang-AWS-Lambda-Example
+![build](https://github.com/mathisve/golang-AWS-Lambda-Example/actions/workflows/go.yaml/badge.svg?branch=master)
 
 This is the progress from [this](https://www.youtube.com/watch?v=pM03-lKt8BI)
 
--- main.go
+++ main.go
@@ -1,66 +1,70 @@
 package main
 
 import (
-  "fmt"
-  "github.com/aws/aws-lambda-go/lambda"
+	"bytes"
+	"io/ioutil"
+	"log"
+	"net/http"
 
-  "github.com/aws/aws-sdk-go/aws"
-  "github.com/aws/aws-sdk-go/aws/session"
-  "github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-lambda-go/lambda"
 
-  "net/http"
-  "io/ioutil"
-  "bytes"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 )
 
 var s3session *s3.S3
 
 const (
-  REGION = "eu-west-1"
-  BUCKET_NAME = "lambdatestyoutube"
+	REGION      = "eu-west-1"
+	BUCKET_NAME = "lambdatestyoutube"
 )
 
+type InputEvent struct {
+	Link string `json:"link"`
+	Key  string `json:"key"`
+}
+
 func init() {
-  s3session = s3.New(session.Must(session.NewSession(&aws.Config{
-      Region: aws.String(REGION),
-    })))
+	// create a new S3 Session
+	s3session = s3.New(session.Must(session.NewSession(&aws.Config{
+		Region: aws.String(REGION),
+	})))
 }
 
 func main() {
-  lambda.Start(Handler)
+	// start the lambda handler
+	lambda.Start(Handler)
 }
 
-func Handler(event InputEvent) (string, error) {
-  image := GetImage(event.Link)
-  _, err := s3session.PutObject(&s3.PutObjectInput{
-    Body: bytes.NewReader(image),
-    Bucket: aws.String(BUCKET_NAME),
-    Key: aws.String(event.Key),
-  })
+func Handler(event InputEvent) (int, error) {
+	// get the image
+	image := GetImage(event.Link)
 
-  if err != nil {
-    return "Something went wrong!", err
-  }
+	// upload it to s3 using PutObject
+	_, err := s3session.PutObject(&s3.PutObjectInput{
+		Body:   bytes.NewReader(image),
+		Bucket: aws.String(BUCKET_NAME),
+		Key:    aws.String(event.Key),
+	})
+	if err != nil {
+		return http.StatusInternalServerError, err
+	}
 
-  return "Everything worked YAY!", err
-}
-
-type InputEvent struct {
-  Link string `json:"link"`
-  Key  string `json:"key"`
+	return http.StatusOK, err
 }
 
 func GetImage(url string) (bytes []byte) {
-  resp, err := http.Get(url)
-  if err != nil {
-    fmt.Println(err)
-  }
-  defer resp.Body.Close()
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Println(err)
+	}
+	defer resp.Body.Close()
 
-  bytes, err = ioutil.ReadAll(resp.Body)
-  if err != nil {
-    fmt.Println(err)
-  }
+	bytes, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Println(err)
+	}
 
-  return bytes
+	return bytes
 }
